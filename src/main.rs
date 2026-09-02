0601a836368381745e08e19db8ceb81128c912f7
Atul Nale <aryannale@gmail.com>
2026-09-03 02:00:39 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,17 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim_end().to_string();
-        if command == "exit" {
-            break;
-        } else if command.starts_with("echo") {
-            println!("{}", &command[5..]);
-        } else {
-            println!("{}: command not found", command.trim_end());
+        let mut parse = command.splitn(2, char::is_whitespace);
+        let cmd = parse.next().unwrap();
+        let args = parse.next().unwrap_or("").trim_start();
+        match cmd {
+            "exit" => break,
+            "echo" => println!("{args}"),
+            "type" => match args {
+                "type" | "echo" | "exit" => println!("{} is a shell builtin", args),
+                _ => println!("{}: command not found", args),
+            },
+            _ => println!("{}: command not found", cmd),
         }
     }
 }

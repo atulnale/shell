7185edb2f8a872d47cc9090cae510412586ff04a
Atul Nale <aryannale@gmail.com>
2026-09-03 01:22:53 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+
         if command.trim_end() == String::from("exit") {
             break;
         }
-        println!("{}: command not found", command.trim_end());
+        if command.starts_with("echo") {
+            println!("{}", &command[5..]);
+        } else {
+            println!("{}: command not found", command.trim_end());
+        }
     }
 }

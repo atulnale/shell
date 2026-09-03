00c9fb79f366454bec31264e3809a6859d1fe3cf
Atul Nale <aryannale@gmail.com>
2026-09-03 15:55:54 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
 
     fn check_executable(args: &str) {
         let path = env::var("PATH").unwrap();
-        let mut paths: Vec<&str> = path.split(":").collect();
+        let paths: Vec<&str> = path.split(":").collect();
         for dir in paths {
             let file = Path::new(dir).join(args);
             if file.is_file() {

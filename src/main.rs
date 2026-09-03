f78da74741424a5dd24db04f9b14f021886601b7
Atul Nale <aryannale@gmail.com>
2026-09-03 15:46:46 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{env, path::Path};
 
 fn main() {
     loop {
@@ -8,6 +9,9 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim_end().to_string();
+        if command == "" {
+            continue;
+        }
         let mut parse = command.splitn(2, char::is_whitespace);
         let cmd = parse.next().unwrap();
         let args = parse.next().unwrap_or("").trim_start();
@@ -16,9 +20,22 @@ fn main() {
             "echo" => println!("{args}"),
             "type" => match args {
                 "type" | "echo" | "exit" => println!("{} is a shell builtin", args),
-                _ => println!("{}: not found", args),
+                _ => check_executable(args),
             },
             _ => println!("{}: command not found", cmd),
         }
+    }
+
+    fn check_executable(args: &str) {
+        let path = env::var("PATH").unwrap();
+        let mut paths: Vec<&str> = path.split(":").collect();
+        for dir in paths {
+            let file = Path::new(dir).join(args);
+            if file.is_file() {
+                println!("{} is {}", args, file.to_str().unwrap())
+                return;
+            }
+        }
+        println!("{}: not found", args);
     }
 }
